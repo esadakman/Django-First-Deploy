80f836e9c13dc2bdb31c373db93612a85ff36924
esadakman <esadd26@gmail.com>
2022-08-25 11:25:14 +0300

    procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Django-Study.wsgi --log-file -
+web: gunicorn firstdjangodeploy.wsgi --log-file -
--- main/settings.py
+++ main/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['firstdjangodeploy.herokuapp.com',
-#                  'django-first-deploy-cli.herokuapp.com' '127.0.0.1']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['firstdjangodeploy.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
