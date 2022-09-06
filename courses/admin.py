1be9bbe4be24f406a9ba925d55361ce202d4123e
esadakman <esadd26@gmail.com>
2022-09-06 13:34:00 +0300

    repo reset

--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from courses.models import Category
-from .models import Category, Courses
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Courses)
--- courses/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import home, course_detail
-
-urlpatterns = [
-    path("", home, name='anasayfa'),
-    path('<int:id>', course_detail, name="course_detail")
-]
